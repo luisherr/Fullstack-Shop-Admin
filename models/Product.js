3a82e567f8ba139bdb7ace8ac830430616978034
Luis Herrera <luis378962@gmail.com>
2023-04-28 23:24:16 -0600

    bug fixed on product

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@ import { Schema, model} from 'mongoose';
 
 const ProductSchema = new Schema({
     title: {type: String, required: true},
-    description: String,
+    description: { type: String, required: true},
     price: { type: Number, required: true},
 });
 
