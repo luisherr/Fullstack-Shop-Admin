be9fecd2fa4ae1fac4d81dd9551e48b62153cd61
Luis Herrera <luis378962@gmail.com>
2023-05-15 17:15:27 -0600

    edit product agregado

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,15 +1,22 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function NewProduct() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
+    const [goToProducts, setGoToProducts] = useState(false);
+    const router = useRouter();
     async function createProduct(ev)  {
         ev.preventDefault();
         const data = { title, description, price};
         await axios.post('/api/products', data);
+        setGoToProducts(true);
+    }
+    if (goToProducts) {
+        return router.push('/products'); 
     }
     return (
         <Layout>
